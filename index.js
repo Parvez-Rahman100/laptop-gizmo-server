c58e3d532ba187f9de37b93a8230ee1428dcff1a
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-05-22 17:22:22 +0600

    initial project setup done

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@laptopgizmo.k2j6o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run (){
+    try{
+        await client.connect();
+        const partsCollection = client.db('laptopGizmo').collection('parts');
+        console.log('db connected');
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req,res)=>{
+    res.send('running')
+})
+
+
+
+app.listen(port,()=>{
+    console.log('listenning to port', port);
+})
