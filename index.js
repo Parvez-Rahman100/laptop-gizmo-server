73db89efb9620ece2042b60edfe15796ab53e5f2
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-05-26 21:54:18 +0600

    delete method applied

--- index.js
+++ index.js
@@ -55,6 +55,15 @@ async function run (){
             res.send(result)
         })
 
+        
+        app.delete('/parts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await partsCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
         app.get('/parts/:id',async(req,res)=>{
             const id = req.params.id;
             const query = {_id : ObjectId(id)};
